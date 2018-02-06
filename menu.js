98c5c540fa1900ae8d8ba58cec22d38590d6fd38
Khozin Viacheslav <khozin.slavik@gmail.com>
2018-02-06 02:25:46 +0200

    latest version

--- menu.js
+++ menu.js
@@ -1,97 +1,74 @@
-function Menu(options) {
-    var elem;  // root element
-    var hideTimer; // timer for hidding
+var hideTimer;
 
-    function getElem() {
-      if (!elem) render();
-      return elem;
-    }
-    /*
-      create root element and add event handlers
-  */
-    function render() {
-      elem = document.createElement('div');
-      elem.className = "menu";
-  
-      var titleElem = document.createElement('span');
-      elem.appendChild(titleElem);
-      titleElem.className = "menu__title";
-      titleElem.textContent = options.title;
-  
-      elem.onmousedown = function() {
-        return false;
-      };
-      
+function showMenu(event){
+    var target = event.target;
+    /* if menu does'nt exist, сreate it and set position  */
+    if (target.classList.contains('open')) 
+      return;
+    var menu = document.createElement('div');
 
-      elem.onmouseover = function(event) {
-        if (event.target.closest('.menu__title')) {
-          open();
-        }
-        if (event.target.closest('li')) {
-          clearTimeout(hideTimer);
-        }
-        
-      }
+    renderMenu();
+    setPosition();
+    target.classList.add('open');
 
-      elem.onmouseout = function(event){
-        if (event.target.closest('.menu__title')) {
-          hideTimer = setTimeout(function(){close()},300);
-        }
-      }
-  
-    }
-  /*
-    create drop-dowm list
-    and we will place it depending on the free space
-  */
-    function renderItems() {
-      var items = options.items || [];
-      var list = document.createElement('ul');
-      items.forEach(function(item) {
+    /* add events to catch the mouse */
+
+    menu.addEventListener('mouseout',closeMenu);
+    target.onmouseout = closeMenu;
+    menu.addEventListener('mouseover',keepMenu);
+
+    /* create menu from array*/
+    function renderMenu() {
+      var ul = document.createElement('ul')
+      menuText.forEach(function(item) {
         var li = document.createElement('li');
-        li.textContent = item;
-        list.appendChild(li);
+        var a = document.createElement('a');
+        a.href = '#';
+        a.text = item;
+        li.appendChild(a);
+        ul.appendChild(li);
       });
+      menu.appendChild(ul);
+      document.body.appendChild(menu);
+      menu.classList.add('contextMenu');
+    };
 
-      elem.appendChild(list);
-
-      // define the current coordinates
-
-      var coords = elem.getBoundingClientRect();
-      var left = coords.left + (elem.offsetWidth - list.offsetWidth) / 2;
-
-      if (left < 0) left = 0; 
-      var top = coords.top - list.offsetHeight + 20;
-      
-      if (top < 0) { 
-        top = coords.top + elem.offsetHeight - 20;
+    /* calculate position and set it*/
+    function setPosition(){
+      var coords = target.getBoundingClientRect();
+      var left = coords.left;
+      var top = coords.top + target.offsetHeight + 5;
+      var bottom = coords.top + target.offsetHeight + menu.offsetHeight ;
+      if (bottom > document.documentElement.clientHeight ) { 
+        top = coords.top - menu.offsetHeight - 5;
       }
-      list.style.left = left + 'px';
-      list.style.top = top + 'px';
-      
+      if (top < 0) {
+        top = coords.top + target.offsetHeight/2 - menu.offsetHeight/2;
+        left = coords.left - menu.offsetWidth - 5;
+      }
+      if (left < 0) {
+        left = coords.left + target.offsetWidth + 5;
+      }
+      menu.style.left = left + 'px';
+      menu.style.top = top + 'px';
+    }   
+
+    /* event handlers */
+    function keepMenu(){
+      console.log('keepMenu timer' + hideTimer);
+      clearTimeout(hideTimer);
+
     }
-  
-    function open() {
-      if (!elem.querySelector('ul')) {
-        renderItems();
-      }
-      elem.classList.add('open');
-      elem.classList.add('tooltip');
-
-    };
-  
-    function close() {
-      elem.classList.remove('open');
-      elem.classList.add('tooltip');
-    };
-  
-    function toggle() {
-      if (elem.classList.contains('open')) close();
-      else open();
-    };
-  
-    // make the methods public
-    this.getElem = getElem;
-    this.close = close;
-    this.open = open;
+    function closeMenu(){
+      if (!target.classList.contains('open')) return;
+      hideTimer = setTimeout(function(){        
+        target.classList.remove('open');
+        var menuCont = document.getElementsByClassName('contextMenu')[0];
+        if (menuCont) {
+          document.body.removeChild(menuCont);
+        };
+        target.classList.remove('open');
+      },300);
+      console.log('closeMenu timer' + hideTimer);
+    }
   }
